0313b1a06f8142d3c60a9fbd77169723f06c91ee
cholid.dnr <cholid.dnr@gmail.com>
2025-05-30 21:24:33 +0700

    uncommect server middleware

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,21 +1,21 @@
 export default defineEventHandler(async (e) => {
   const url = getRequestURL(e);
-  // if (
-  //   url.pathname.startsWith("/api/merchants") ||
-  //   url.pathname.startsWith("/api/products") ||
-  //   url.pathname.startsWith("/api/questions") ||
-  //   url.pathname.startsWith("/api/statistics") ||
-  //   url.pathname.startsWith("/api/user")
-  // ) {
-  //   const session = await auth(e).api.getSession({
-  //     headers: e.headers,
-  //   });
-  //   if (!session) {
-  //     return sendError(
-  //       e,
-  //       createError({ statusCode: 401, statusMessage: "Unauthorized" })
-  //     );
-  //   }
-  // }
+  if (
+    url.pathname.startsWith("/api/merchants") ||
+    url.pathname.startsWith("/api/products") ||
+    url.pathname.startsWith("/api/questions") ||
+    url.pathname.startsWith("/api/statistics") ||
+    url.pathname.startsWith("/api/user")
+  ) {
+    const session = await auth(e).api.getSession({
+      headers: e.headers,
+    });
+    if (!session) {
+      return sendError(
+        e,
+        createError({ statusCode: 401, statusMessage: "Unauthorized" })
+      );
+    }
+  }
   return;
 });
