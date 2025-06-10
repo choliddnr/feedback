0613fc50e9d3f7e7124256affaa2ae5f10870b2b
cholid.dnr <cholid.dnr@gmail.com>
2025-06-10 21:29:13 +0700

    fix: logic issue

--- server/api/user/[id].patch.ts
+++ server/api/user/[id].patch.ts
@@ -10,10 +10,13 @@ export default defineEventHandler(async (e: H3Event) => {
   const body = parseMultipartData(await readMultipartFormData(e));
 
   let newData = {} as Partial<User>;
-  newData["username"] = body.username;
-  newData["name"] = body.name;
-  newData["defaultMerchant"] = Number(body.defaultMerchant);
+  if (body.username) newData["username"] = body.username;
+  if (body.name) newData["name"] = body.name;
+  if (body.defaultMerchant)
+    newData["defaultMerchant"] = Number(body.defaultMerchant);
   const validate = UpdateUserSchema.safeParse(newData);
+
+  console.log("validation", validate);
   if (!validate.success) {
     return sendError(
       e,
@@ -24,22 +27,26 @@ export default defineEventHandler(async (e: H3Event) => {
       })
     );
   }
-
+  console.log("validation 2", validate);
   try {
-    const username = await db(e)
-      .select()
-      .from(user)
-      .where(eq(user.username, newData["username"]))
-      .limit(1)
-      .get();
-    if (username) {
-      return sendError(
-        e,
-        createError({
-          statusCode: 422,
-          statusMessage: "username is already taken",
-        })
-      );
+    if (newData["username"]) {
+      const username = await db(e)
+        .select()
+        .from(user)
+        .where(eq(user.username, newData["username"]))
+        .limit(1)
+        .get();
+      console.log("username", username);
+      if (username) {
+        return sendError(
+          e,
+          createError({
+            statusCode: 422,
+            statusMessage: "username is already taken",
+          })
+        );
+      }
+      console.log("body", body, username);
     }
 
     const oldData = await db(e)
@@ -48,6 +55,7 @@ export default defineEventHandler(async (e: H3Event) => {
       .where(eq(user.id, id))
       .limit(1)
       .get();
+
     if (!oldData) {
       return sendError(
         e,
@@ -58,15 +66,7 @@ export default defineEventHandler(async (e: H3Event) => {
         })
       );
     }
-
     if (body.image) {
-      /**
-       * logic to update user image when using bunny.net storage
-       * Since we are using bunny.net storage that require raw File data,
-       * we used readFormData to get the file data
-       * and then upload it to the storage
-       */
-
       /**
        * Add delete old image logic
        * If the logo is updated, we need to delete the old logo image
@@ -78,7 +78,7 @@ export default defineEventHandler(async (e: H3Event) => {
       await saveImg(e, body.image.data, filename); // all uploaded images are saved as webp format
       newData["image"] = filename;
     }
-
+    console.log("newData update user", newData);
     await db(e).update(user).set(newData).where(eq(user.id, id));
     return auth(e).api.getSession({
       headers: e.headers,
