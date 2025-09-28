72e3020e32a694c488140dfc7cc493aa2bcff2ce
choliddnr <cholid.dnr@gmail.com>
2025-09-28 07:52:15 +0700

    test deployment 1

--- app/pages/auth/signin.vue
+++ app/pages/auth/signin.vue
@@ -1,56 +1,56 @@
 <script setup lang="ts">
-import * as z from 'zod';
-import type { FormSubmitEvent } from '@nuxt/ui';
+import * as z from "zod";
+import type { FormSubmitEvent } from "@nuxt/ui";
 
-import { authClient } from '@/utils/client'; // import the auth client
+import { authClient } from "@/utils/client"; // import the auth client
 
 const toast = useToast();
 
 const fields = [
   {
-    name: 'email',
-    type: 'text' as const,
-    label: 'Email',
-    placeholder: 'Enter your email',
+    name: "email",
+    type: "text" as const,
+    label: "Email",
+    placeholder: "Enter your email",
     required: true,
   },
   {
-    name: 'password',
-    label: 'Password',
-    type: 'password' as const,
-    placeholder: 'Enter your password',
+    name: "password",
+    label: "Password",
+    type: "password" as const,
+    placeholder: "Enter your password",
   },
   {
-    name: 'remember',
-    label: 'Remember me',
-    type: 'checkbox' as const,
+    name: "remember",
+    label: "Remember me",
+    type: "checkbox" as const,
     defaultValue: false,
   },
 ];
 
 const providers = [
   {
-    label: 'Google',
-    icon: 'i-simple-icons-google',
+    label: "Google",
+    icon: "i-simple-icons-google",
     onClick: async () => {
       const _data = await authClient.signIn.social(
         {
-          provider: 'google',
+          provider: "google",
         },
         {
           onSuccess: () => {
-            toast.add({ title: 'Google', description: 'Login with Google' });
-            navigateTo('/admin');
+            toast.add({ title: "Google", description: "Login with Google" });
+            navigateTo("/admin");
           },
-        },
+        }
       );
     },
   },
 ];
 
 const schema = z.object({
-  email: z.string().email('Invalid email'),
-  password: z.string().min(8, 'Must be at least 8 characters'),
+  email: z.string().email("Invalid email"),
+  password: z.string().min(8, "Must be at least 8 characters"),
   remember: z.boolean(),
 });
 
@@ -70,7 +70,7 @@ const onSubmit = async (payload: FormSubmitEvent<Schema>) => {
       /**
        * A URL to redirect to after the user verifies their email (optional)
        */
-      callbackURL: '/admin',
+      callbackURL: "/admin",
       /**
        * remember the user session after the browser is closed.
        * @default true
@@ -85,13 +85,13 @@ const onSubmit = async (payload: FormSubmitEvent<Schema>) => {
       },
       onSuccess: (_ctx) => {
         // redirect to the dashboard or sign in page
-        navigateTo('/admin');
+        navigateTo("/admin");
       },
       onError: (ctx) => {
         // display the error message
         alert(ctx.error.message);
       },
-    },
+    }
   );
 };
 </script>
@@ -113,7 +113,7 @@ const onSubmit = async (payload: FormSubmitEvent<Schema>) => {
           Don't have an account?
           <ULink to="/auth/signup" class="text-primary font-medium"
             >Sign up</ULink
-          >.
+          >. github build
         </template>
       </UAuthForm>
     </UPageCard>
