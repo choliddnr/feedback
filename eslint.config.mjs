260af79ca3b1d5c6a65c67a01ac0116b549bfe04
choliddnr <cholid.dnr@gmail.com>
2025-10-01 07:00:14 +0700

    remove unused files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,9 @@
 import withNuxt from "./.nuxt/eslint.config.mjs";
-import tseslint from "typescript-eslint";
 
 export default withNuxt(
   // TypeScript rules
-  ...tseslint.configs.recommended,
   {
     files: ["**/*.ts", "**/*.tsx", "**/*.vue"],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        project: "./tsconfig.json", // optional: only if you want type-aware linting
-        extraFileExtensions: [".vue"],
-      },
-    },
-    plugins: {
-      "@typescript-eslint": tseslint.plugin,
-    },
     rules: {
       "@typescript-eslint/no-unused-vars": [
         "warn",
