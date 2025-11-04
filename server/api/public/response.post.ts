c0ca5915ecf0fbe7fd17bd96079fdea53d7e3f5a
choliddnr <cholid.dnr@gmail.com>
2025-11-05 01:15:38 +0700

    remove transaction query

--- server/api/public/response.post.ts
+++ server/api/public/response.post.ts
@@ -55,128 +55,128 @@ export default defineEventHandler(async (e) => {
     }
   });
 
-  if (import.meta.dev) {
-    /**
-     * Running on development
-     * @returns
-     */
-    const runTransactionAsync = async () => {
-      return await Promise.resolve(
-        (db(e) as DB).transaction(
-          (tx: DB) => {
-            const answers = [] as {
-              response: number;
-              question: number;
-              answer: string;
-            }[];
-            const products_to_responses_data = [] as {
-              product_id: number;
-              response_id: number;
-            }[];
-            const product_list = [] as number[];
+  // if (import.meta.dev) {
+  //   /**
+  //    * Running on development
+  //    * @returns
+  //    */
+  //   const runTransactionAsync = async () => {
+  //     return await Promise.resolve(
+  //       (db(e) as DB).transaction(
+  //         (tx: DB) => {
+  //           const answers = [] as {
+  //             response: number;
+  //             question: number;
+  //             answer: string;
+  //           }[];
+  //           const products_to_responses_data = [] as {
+  //             product_id: number;
+  //             response_id: number;
+  //           }[];
+  //           const product_list = [] as number[];
 
-            const respondent_id = tx
-              .insert(respondents)
-              .values(body.respondent)
-              .returning({ inserted_id: respondents.id })
-              .get();
-            const response_id = tx
-              .insert(responses)
-              .values({
-                respondent: respondent_id.inserted_id,
-                merchant: Number(body.merchant),
-              })
-              .returning({ inserted_id: responses.id })
-              .get();
+  //           const respondent_id = tx
+  //             .insert(respondents)
+  //             .values(body.respondent)
+  //             .returning({ inserted_id: respondents.id })
+  //             .get();
+  //           const response_id = tx
+  //             .insert(responses)
+  //             .values({
+  //               respondent: respondent_id.inserted_id,
+  //               merchant: Number(body.merchant),
+  //             })
+  //             .returning({ inserted_id: responses.id })
+  //             .get();
 
-            Object.keys(body.answers).forEach((key) => {
-              const ids = parseKey(key);
-              answers.push({
-                response: response_id.inserted_id,
-                question: ids[1],
-                answer: body.answers[key],
-              });
-              if (!product_list.includes(ids[0])) {
-                products_to_responses_data.push({
-                  product_id: ids[0],
-                  response_id: response_id.inserted_id,
-                });
-                product_list.push(ids[0]);
-              }
-            });
-            tx.insert(response_answers).values(answers).run();
-            tx.insert(products_to_responses)
-              .values(products_to_responses_data)
-              .run();
-          },
-          {
-            behavior: "deferred",
-          }
-        )
-      );
-    };
-    await runTransactionAsync();
-  } else {
-    /**
-     * Since D1 does'nt support transactions, to ensure ACID properties,
-     * we will perform the operations sequentially. and save each successfull state into array.
-     * If any operation fails, we will rollback the changes by deleting the inserted data saved in the array.
-     */
-    const insertedData: {
-      inserted_id: number;
-      table: SQLiteTableWithColumns<any>;
-    }[] = [];
+  //           Object.keys(body.answers).forEach((key) => {
+  //             const ids = parseKey(key);
+  //             answers.push({
+  //               response: response_id.inserted_id,
+  //               question: ids[1],
+  //               answer: body.answers[key],
+  //             });
+  //             if (!product_list.includes(ids[0])) {
+  //               products_to_responses_data.push({
+  //                 product_id: ids[0],
+  //                 response_id: response_id.inserted_id,
+  //               });
+  //               product_list.push(ids[0]);
+  //             }
+  //           });
+  //           tx.insert(response_answers).values(answers).run();
+  //           tx.insert(products_to_responses)
+  //             .values(products_to_responses_data)
+  //             .run();
+  //         },
+  //         {
+  //           behavior: "deferred",
+  //         }
+  //       )
+  //     );
+  //   };
+  //   await runTransactionAsync();
+  // } else {
+  /**
+   * Since D1 does'nt support transactions, to ensure ACID properties,
+   * we will perform the operations sequentially. and save each successfull state into array.
+   * If any operation fails, we will rollback the changes by deleting the inserted data saved in the array.
+   */
+  const insertedData: {
+    inserted_id: number;
+    table: SQLiteTableWithColumns<any>;
+  }[] = [];
 
-    try {
-      const respondent = await db(e)
-        .insert(respondents)
-        .values(body.respondent)
-        .returning()
-        .get();
-      insertedData.push({ inserted_id: respondent.id, table: respondents });
+  try {
+    const respondent = await db(e)
+      .insert(respondents)
+      .values(body.respondent)
+      .returning()
+      .get();
+    insertedData.push({ inserted_id: respondent.id, table: respondents });
 
-      const response = await db(e)
-        .insert(responses)
-        .values({
-          respondent: respondent.id,
-          merchant: Number(body.merchant),
-        })
-        .returning()
-        .get();
-      insertedData.push({ inserted_id: response.id, table: responses });
+    const response = await db(e)
+      .insert(responses)
+      .values({
+        respondent: respondent.id,
+        merchant: Number(body.merchant),
+      })
+      .returning()
+      .get();
+    insertedData.push({ inserted_id: response.id, table: responses });
 
-      const answers = [] as {
-        response: number;
-        question: number;
-        answer: string;
-      }[];
+    const answers = [] as {
+      response: number;
+      question: number;
+      answer: string;
+    }[];
 
-      Object.keys(body.answers).forEach((key) => {
-        const ids = parseKey(key);
-        answers.push({
-          response: response.id,
-          question: ids[1],
-          answer: body.answers[key],
-        });
+    Object.keys(body.answers).forEach((key) => {
+      const ids = parseKey(key);
+      answers.push({
+        response: response.id,
+        question: ids[1],
+        answer: body.answers[key],
       });
+    });
 
-      await db(e).insert(response_answers).values(answers).run();
-    } catch (error) {
-      // rollback logic
-      for (const data of insertedData) {
-        await db(e)
-          .delete(data.table)
-          .where(eq(data.table.id, data.inserted_id))
-          .run();
-      }
-      return sendError(
-        e,
-        createError({
-          statusCode: 500,
-          statusMessage: "Failed to process response!",
-        })
-      );
+    await db(e).insert(response_answers).values(answers).run();
+  } catch (error) {
+    // rollback logic
+    for (const data of insertedData) {
+      await db(e)
+        .delete(data.table)
+        .where(eq(data.table.id, data.inserted_id))
+        .run();
     }
+    return sendError(
+      e,
+      createError({
+        statusCode: 500,
+        statusMessage: "Failed to process response!",
+      })
+    );
   }
+  // }
   return { success: true, message: "Response procecced successfully." };
 });
