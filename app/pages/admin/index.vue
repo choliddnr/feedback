a9d7c695376f87c1d63cc6df8c8eef84efd6d1bb
choliddnr <cholid.dnr@gmail.com>
2025-12-31 15:42:57 +0700

    fix bug 2

--- app/pages/admin/index.vue
+++ app/pages/admin/index.vue
@@ -68,9 +68,10 @@ const period = ref<Period>("daily");
     </template>
 
     <template #body>
-      <AdminDashboard />
+      <!-- <AdminDashboard /> -->
 
-      <AdminHomeStats :period="period" :range="range" />
+      <!-- <AdminHomeStats :period="period" :range="range" /> -->
+       
       <!-- <HomeChart :period="period" :range="range" /> -->
       <!-- <AdminHomeResponseAnalysis /> -->
     </template>
